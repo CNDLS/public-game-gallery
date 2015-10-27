992c042cfa2ca0f90bb24bc075aac325e1773565
CNDLS <cndls_developers@georgetown.edu>
2015-10-27 09:16:04 -0400

    got demo game working again. need to simplify round state callbacks.

--- custom_js/demo.js
+++ custom_js/demo.js
@@ -3,17 +3,30 @@
 * To keep things simple, we treat the characters as set pieces that get moved about.
 */
 
-Game.Scene.new(Game.Scene.Basic, "Gorge", { 
+Game.Scene.new(Game.Scene.Basic, "Gorge", {
 
+	finalize: function (round) {
+    $("h1#title").addClass("dim");
+  },
+  
+  enterListenForPlayer: function (evt, info) {
+    var round = info.round;
+    var listener = round.listener;
+  	$(listener.container).find("input[type=text]").get(0).focus();
+  },
+  
 	leaveListenForPlayer: function (evt, info) {
 		var round = info.round;
 		var game = round.game;
 		var answer = info.args[0];
 		var score = info.args[1];
 		var subject = $("li.knight, li.king").first();
+    
+    Game.clearCards("li.old_man", ".prompt");
 		
 		switch (score) {
 			case 0: // a wrong answer.
+			case undefined: // a timeout.
 				throwTheSubjectIntoTheGorge(info, subject, round, game);
 				// wait before transitioning out of this state.
 				info.continue = false;
@@ -21,42 +34,59 @@ Game.Scene.new(Game.Scene.Basic, "Gorge", {
 		}
 	},
 				
-	leaveListenForPlayer: function (evt, info) {
+	enterRespondToPlayer: function (evt, info) {
 		var round = info.round;
 		var game = round.game;
 		var responder = round.responder;
-		var answer = info.args[0];
-		var score = info.args[1];
+		var answer = responder.answer;
+		var score = responder.score;
 		var subject = $("li.knight, li.king").first();
 		var old_man = $("li.old_man");
+		var old_man_response_bubble = $("li.old_man .card.responder");
 		var _this = this;
-    
+		
 		switch (score) {
 			case 1:  // a correct answer.
 				if (responder.cards.length) {
 					// if there is a response, make it clickable and clear other cards.
 					Game.clearCards("li.answers");
-					Game.clearCards("li.old_man", ".prompt");
 
 					// make the old man's answer feedback voice balloon clickable,
 					// and use the click to send either a subject or him into the gorge.
-      		var old_man_response_bubble = $("li.old_man .card.responder");
-          console.log(old_man_response_bubble)
-					old_man_response_bubble.find("button")
+					old_man_response_bubble.find("button.continue")
 					.click(function () {
-            debugger;
 						if (subject.hasClass("king")) {
 							Game.clearCards("li.old_man", ".responder");
 							throwTheOldManIntoTheGorge(old_man, round, game);
+							game.theKingHasCrossedTheBridge = true;
 						} else {
 							letTheSubjectWalkAcrossTheBridge(subject, round);
 						}
 					});
-					info.continue = false;
 				}
 				break;
 		}
 	},
+  
+	leaveRespondToPlayer: function (evt, info) {
+		var round = info.round;
+		var game = round.game;
+		var responder = round.responder;
+		var answer = responder.answer;
+		var score = responder.score;
+		var subject = $("li.knight, li.king").first();
+		var old_man = $("li.old_man");
+		var old_man_response_bubble = $("li.old_man .card.responder");
+		var _this = this;
+    
+		switch (score) {
+			case 1:  // a correct answer.
+				if (responder.cards.length) {
+					info.continue = false;
+				}
+				break;
+		}
+  },
 
 	enterGivePrompt: function (evt, info) {
 		if ($("li.knight, li.king").length == 0) {
@@ -68,6 +98,7 @@ Game.Scene.new(Game.Scene.Basic, "Gorge", {
 			});
 		}
 	}
+	
 });
 
 
@@ -85,6 +116,8 @@ function throwTheSubjectIntoTheGorge (info, subject, round, game) {
 	var _this = this;
 	round.responder.addPromise(this.dfd.promise());
 
+	Game.clearCards();
+
 	subject
 	// *** START ANIM.*** hoist him up,
 	.animate({ "top": -300 }, { duration: 1000, easing: "swing",
@@ -95,12 +128,18 @@ function throwTheSubjectIntoTheGorge (info, subject, round, game) {
 			.animate({ "opacity": 0 }, { duration: 1000, queue: false })
 			.animate({ "top": 300 }, { duration: 1000, easing: "swing",
 				done: function subjectIsGone () {
-					// abort this round, and advance to the next one.
-					subject.remove();
 					_this.dfd.resolve();
 					round.cancelTransition();
-					// we pass next desired round through abort() to the game.
-					round.abort(game.spec.Rounds[0], Game.clearCards);
+					
+					// if the 'subject' is the king, end the game.
+					if (subject.hasClass("king")) {
+						subject.remove();
+						game.end();
+					} else {
+						// we pass next desired round through abort() to the game.
+						subject.remove();
+						round.abort(game.spec.Rounds[0]);
+					}
 				} // end subjectIsGone.
 			});
 		} // end subjectIsUp.
@@ -109,8 +148,9 @@ function throwTheSubjectIntoTheGorge (info, subject, round, game) {
 
 
 function throwTheOldManIntoTheGorge (old_man, round, game) {
+	var _this = this;
+	
 	old_man
-	// *** START ANIM.*** hoist him up,
 	.animate({ "top": -300 }, { duration: 1000, easing: "swing",
 		done: function oldManIsUp() {
 			// drop him down,
@@ -120,15 +160,8 @@ function throwTheOldManIntoTheGorge (old_man, round, game) {
 			.animate({ "opacity": 0 }, { duration: 1000, queue: false })
 			.animate({ "top": 300 }, { duration: 1000, easing: "swing",
 				done: function oldManIsGone() {
-					// end the game.
 					old_man.remove();
-					round.cancelTransition();
-					// after one clock tick (to allow any cleanup), end the game.
-					game.nextTick().then(function endTheGame() {
-						Game.clearCards("li.old_man", ".prompt");
-						Game.clearCards("li.answers");
-						game.end();
-					});
+					_this.letTheSubjectWalkAcrossTheBridge($(".king"), round);
 				} // end oldManIsGone.
 			});
 		} // end oldManIsUp.
@@ -137,7 +170,6 @@ function throwTheOldManIntoTheGorge (old_man, round, game) {
 
 
 function letTheSubjectWalkAcrossTheBridge(subject, round) {
-  debugger;
 	subject.addClass("walker")
 	// *** START ANIM.*** walk him to the middle of the bridge,
 	.animate({ "left": 2 }, { duration: 2000, queue: false }, "swing")
@@ -148,10 +180,25 @@ function letTheSubjectWalkAcrossTheBridge(subject, round) {
 			subject.animate({ "top": -6 }, { duration: 1000, easing: "swing",
 				// move on.
 				done: function subjectIsGone() {
+					if (subject.hasClass("king")) {
+						this.theKingHasCrossedTheBridge = true;
+					}
 					subject.remove();
 					round.transition();
 				} // end subjectIsGone.
 			});
 		} // end subjectIsInTheMiddleOfTheBridge.
 	});
+}
+
+
+/***
+ * Winning. Remember, this must return a *function* that will get executed 
+ * and will evaluate the player's overall success.
+ ***/
+Game.TheKingGotAcross = function () {
+	return function () {
+		// here is the evaluation really happening. just read a var.
+		return game.theKingHasCrossedTheBridge;
+	}
 }
